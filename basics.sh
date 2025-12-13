2c5c3c78fb866ab1541f21fd3681e21edfd49caf
rangaomkaram <udaykumarsesha@gmail.com>
2025-12-13 06:35:11 +0530

    changes in the file about Shebang

--- basics.sh
+++ basics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# here above first line is mandatory in any shell/bash script -> it is konwn as Shebang, which checks the syntax fo code what we written in workspace . Act as Interpreter.
 # this is comment , can't be executed, just information purpose
 # below print the charcters "Shell Script Practicing or Bash Scripting"
 echo "Shell Script Practing"
