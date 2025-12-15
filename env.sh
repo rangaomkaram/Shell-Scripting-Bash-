ef5e26e21a17604ad1dfeb96e9ba3d0388de7933
rangaomkaram <udaykumarsesha@gmail.com>
2025-12-15 20:52:13 +0530

    environment variables

--- env.sh
+++ env.sh
@@ -4,6 +4,7 @@ echo "Explaining about the Environment Variables"
 
 # syntax : export  is the keyword -> The variable is valid until this process is alive,it is accessible to sub process also.
 
- export Shell_Env="ShellScripting with ENV"
+# eg : export Shell_Env="ShellScripting with ENV"
 
-echo "${Shell_Env} here printing the content"
+# echo "${Shell_Env} here printing the content"
+echo "environmental Variable is: $ENV_VAR"
