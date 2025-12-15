99a4aa4ae375ea1083fd1d9244c97533cded2a53
rangaomkaram <udaykumarsesha@gmail.com>
2025-12-15 18:30:01 +0530

    environment variable to export which is live (when process is alive)

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Explaining about the Environment Variables"
+
+# syntax : export  is the keyword -> The variable is valid until this process is alive,it is accessible to sub process also.
+
+export Shell_Env = "ShellScripting with ENV"
+
+echo "${Shell_Env} here printing the content"
