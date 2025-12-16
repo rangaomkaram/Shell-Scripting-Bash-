259cc6a0e0af0402a89a137c2a029519aa584ebb
rangaomkaram <udaykumarsesha@gmail.com>
2025-12-16 13:42:33 +0530

    all args passed using  and S@ cmds

---
+++ special_varaibles.sh
@@ -0,0 +1,10 @@
+#!/bin /bash
+
+#Special Variables in  Shell Scripting
+
+echo "All variables or args passed to the script using command  : $@ "
+echo "All Variables or args can be passed to the script using command  : $* "
+
+# exec :  sh special_varaibles.sh happy begin and end
+# o/p : All variables or args passed to the script using command  : happy begin and end 
+# o/p : All Variables or args can be passed to the script using command  : happy begin and end 
