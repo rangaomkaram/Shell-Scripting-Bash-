322e8e7167a812882a9f8d0cc1070a2ade111cc1
rangaomkaram <udaykumarsesha@gmail.com>
2025-12-16 13:28:28 +0530

    dynamic execution of variable and covering concept & operator

--- dynamic_Var_Exec.sh
+++ dynamic_Var_Exec.sh
@@ -19,4 +19,16 @@ sleep 10 & # here & operator makes the execution at background
 END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($END_TIME - $START_TIME))
-echo "Script Execution time : $TOTAL_TIME"
+echo "Script Execution time : $TOTAL_TIME" #o/p Script Execution time : 0
+
+# here remove the & operator see the execution below
+
+BEGIN_TIME=$(date +%s)
+
+sleep 10
+
+CLOSE_TIME=$(date +%s)
+
+REMAIN_TIME=$(($CLOSE_TIME - $BEGIN_TIME))
+
+echo "script after removal of & operator execution time : $REMAIN_TIME"
