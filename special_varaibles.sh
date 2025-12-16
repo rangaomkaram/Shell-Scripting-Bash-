3fbd5b8372330ec5f1a42dfcf703994c4895530c
rangaomkaram <udaykumarsesha@gmail.com>
2025-12-16 13:43:51 +0530

    know your script name using /usr/bin/bash cmd

--- special_varaibles.sh
+++ special_varaibles.sh
@@ -8,3 +8,6 @@ echo "All Variables or args can be passed to the script using command  : $* "
 # exec :  sh special_varaibles.sh happy begin and end
 # o/p : All variables or args passed to the script using command  : happy begin and end 
 # o/p : All Variables or args can be passed to the script using command  : happy begin and end 
+
+# To know your script name 
+echo "Your Script name : $0"
