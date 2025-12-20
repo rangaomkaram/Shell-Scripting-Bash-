808ef89eaf28405c2d354cc0c11555522b78e33a
rangaomkaram <udaykumarsesha@gmail.com>
2025-12-20 10:16:01 +0530

    data type in shell scripting

---
+++ data_types.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Data types - Everything is considered as string in shell
+
+NUM1=100
+NUM2=200
+
+SUM=$(($NUM1 + $NUM2))
+echo "Sum of the two numbers  $NUM1 and $NUM2  is : ${SUM}"
+
