46eda29bada736e40dd4c33b641bdbfb7f0137ec
rangaomkaram <udaykumarsesha@gmail.com>
2026-01-23 11:36:51 +0530

    bug fixed

--- conditions.sh
+++ conditions.sh
@@ -29,12 +29,10 @@ fi
 # prime , composite neither prime or composite
 ## && -> Logical AND: True if both conditions are true	[ $a -gt 5 ] && [ $a -lt 10 ]
 
-# if [ [ $((NUMBER3 % 2)) -eq 1 ] &&  [ "$NUMBER3" -gt 1 ] ]; then
-#     echo " ${NUMBER3} is prime number"
-if { [ $((NUMBER3 % 2)) -eq 1 ] && [ "$NUMBER3" -gt 1 ]; } || [ "$NUMBER3" -eq 2 ]; then
-   echo "${NUMBER3} is prime number"
-elif [ [ $((NUMBER3 % 2)) -eq 0 ] || [ "$NUMBER3" -ne 2 ] ]; then
+if [[ ( $((NUMBER3 % 2)) -eq 1 && "$NUMBER3" -gt 1 ) || "$NUMBER3" -eq 2 || "$NUMBER3" -ne 0 ]]; then
+    echo "${NUMBER3} is prime number"
+elif [ $((NUMBER3 % 2)) -eq 0 ] && [ "$NUMBER3" -ne 2 ] && [ "$NUMBER3" -gt 1 ] && [ "$NUMBER" -ne 0 ]; then
     echo "$NUMBER3 is composite Number"
 else
-    echo "$NUMBER3 is Neither Prime nor Composite"
+    echo "$NUMBER3 is neither prime or composite"
 fi
